cbff7fc9234b5b8507b3a90f0512f309c0d6eba9
skinrush <contact@hyperdesign.agency>
2026-04-20 11:18:54 +0100

    Fix Skin model column mappings to match DB

--- models/Skin.js
+++ models/Skin.js
@@ -2,19 +2,78 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../db.js';
 
 export const Skin = sequelize.define('Skin', {
-  name: { type: DataTypes.STRING, field: 'name' },
-  weapon: { type: DataTypes.STRING, field: 'weapon' },
-  collection: { type: DataTypes.STRING, field: 'collection' },
-  rarity: { type: DataTypes.STRING, field: 'rarity' },
-  min_float: { type: DataTypes.FLOAT, field: 'min_float' },
-  max_float: { type: DataTypes.FLOAT, field: 'max_float' },
-  stattrak: { type: DataTypes.BOOLEAN, field: 'stattrak' },
-  souvenir: { type: DataTypes.BOOLEAN, field: 'souvenir' },
-  rarityColor: { type: DataTypes.STRING, field: 'rarity_color' },
-  description: DataTypes.TEXT,
-  tag: DataTypes.STRING,
-  origin: DataTypes.STRING,
-  paintIndex: DataTypes.INTEGER,
+
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'skin_id'
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    field: 'skin_name'
+  },
+
+  weapon: {
+    type: DataTypes.STRING,
+    field: 'weapon_name'
+  },
+
+  rarity: {
+    type: DataTypes.STRING,
+    field: 'rarity_name'
+  },
+
+  category: {
+    type: DataTypes.STRING,
+    field: 'category_name'
+  },
+
+  min_float: {
+    type: DataTypes.FLOAT,
+    field: 'min_float'
+  },
+
+  max_float: {
+    type: DataTypes.FLOAT,
+    field: 'max_float'
+  },
+
+  stattrak: {
+    type: DataTypes.BOOLEAN,
+    field: 'stattrak'
+  },
+
+  souvenir: {
+    type: DataTypes.BOOLEAN,
+    field: 'souvenir'
+  },
+
+  rarityColor: {
+    type: DataTypes.STRING,
+    field: 'rarity_color'
+  },
+
+  paintIndex: {
+    type: DataTypes.INTEGER,
+    field: 'paint_index'
+  },
+
+  image: {
+    type: DataTypes.STRING,
+    field: 'image_url'
+  },
+
+  phase: {
+    type: DataTypes.STRING,
+    field: 'phase'
+  },
+
+  description: {
+    type: DataTypes.TEXT,
+    field: 'description'
+  }
+
 }, {
   tableName: 'skins',
   timestamps: false
